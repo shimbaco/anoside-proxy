297ca10ace10968e787fd0616ea7dd4e88bb68c0
Koji Shinba <bojovs@gmail.com>
2011-12-25 05:52:57 +0900

    ドメインの修正

--- proxy.js
+++ proxy.js
@@ -3,10 +3,10 @@ var httpProxy = require('http-proxy');
 
 httpProxy.createServer({
   router: {
-    'localhost/images': 'localhost:8000',
-    'localhost/javascripts': 'localhost:8000',
-    'localhost/stylesheets': 'localhost:8000',
-    'localhost/favicon.png': 'localhost:8000',
-    'localhost/*': 'localhost:3000'
+    'anoside.com/images': 'localhost:8000',
+    'anoside.com/javascripts': 'localhost:8000',
+    'anoside.com/stylesheets': 'localhost:8000',
+    'anoside.com/favicon.png': 'localhost:8000',
+    'anoside.com/*': 'localhost:3000'
   }
 }).listen(80);
